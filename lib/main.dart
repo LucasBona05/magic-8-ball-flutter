2a7c7c1d29622f84e2d198d103b6f680a55c0433
LucasBona05 <lucas.goncalves@ucsal.edu.br>
2021-03-04 13:45:30 -0300

    Challenge finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            backgroundColor: Colors.blue[900],
+            title: Text('Ask Me Anything'),
+          ),
+          body: Magic8Ball(),
+        ),
       ),
     );
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int ballNumber = 1;
+  void changeNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1; 
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          changeNumber();
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
